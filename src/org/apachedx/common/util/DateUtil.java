cdb4d3b5b5cab9f789842ac397487f45ea1b6a94
duguruiyuan <252563>
2017-02-05 14:44:10 +0800

    deploy to maven center repository

--- src/org/apachedx/common/util/DateUtil.java
+++ src/org/apachedx/common/util/DateUtil.java
@@ -3,8 +3,7 @@ package org.apachedx.common.util;
 /**
  * 
  * @author cai.hongzhi apachdx@gmail.com
- * @version 0.0.1
- * @category Util class
+ * @version 1.1
  *
  */
 public final class DateUtil {
